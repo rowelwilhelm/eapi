7a5359fa4d2ab80675a2f9c2e3f2716901b49c4b
Roel Wilhelm <roelwilhelm@gmail.com>
2018-08-12 17:01:58 +0800

    Migration Create for products and reviews

--- routes/api.php
+++ routes/api.php
@@ -7,6 +7,7 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::apiResource('/products','ProductController');
+
 Route::prefix('/products')->group(function (){
     Route::apiResource('/{product}/reviews','ReviewController');
 });
